f0fb4326696f90ea10d91e9b23d72274a8f98fdb
Adrian Renner <a.renner+git@eosn.de>
2019-02-26 09:50:35 +0100

    use stupid lib in example app

--- app/src/main/java/de/eosn/stupid/lib/MainActivity.kt
+++ app/src/main/java/de/eosn/stupid/lib/MainActivity.kt
@@ -1,12 +1,16 @@
 package de.eosn.stupid.lib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import de.eosn.stupid.Stupid
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.stupidTextView).text = Stupid.forLocal()
     }
 }
